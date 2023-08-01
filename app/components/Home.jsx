e607f7d026412ff300a2940c57b7b8f4fe9a7ed7
Esther Yoo <liletchie@gmail.com>
2023-08-01 16:19:23 -0400

    login view

---
+++ app/components/Home.jsx
@@ -0,0 +1,88 @@
+"use client";
+import React, { useState } from "react";
+
+const HomePage = () => {
+  const [password, setPassword] = useState(true);
+
+  const handlePassword = (pass) => {
+    if (!pass) {
+      setPassword(false);
+    }
+  };
+  return (
+    <div className="flex justify-center items-center mt-40">
+      <div className="w-full max-w-xs">
+        <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+          <div className="mb-4">
+            <label
+              className="block text-gray-700 text-sm font-bold mb-2"
+              for="username"
+            >
+              Username
+            </label>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="username"
+              type="text"
+              placeholder="username"
+            />
+          </div>
+          <div className="mb-6">
+            <label
+              className="block text-gray-700 text-sm font-bold mb-2"
+              for="password"
+            >
+              Password
+            </label>
+
+            {!password ? (
+              <div>
+                <input
+                  className="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+                  id="password"
+                  type="password"
+                  placeholder="password"
+                />
+                <p className="text-red-500 text-xs italic">
+                  Please choose a password.
+                </p>
+              </div>
+            ) : (
+              <div>
+                <input
+                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+                  id="password"
+                  type="password"
+                  placeholder="password"
+                />
+                <p className="text-red-500 text-xs italic hidden ">
+                  Please choose a password.
+                </p>
+              </div>
+            )}
+          </div>
+          <div className="flex items-center justify-between">
+            <button
+              onClick={handlePassword}
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+              type="button"
+            >
+              Sign In
+            </button>
+            <a
+              className="inline-block align-baseline font-bold text-sm text-blue-400 hover:text-blue-500"
+              href="#"
+            >
+              Forgot Password?
+            </a>
+          </div>
+        </form>
+        <p className="text-center text-gray-500 text-xs">
+          &copy;2023 Fakebook Co. All rights reserved.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
---
+++ app/components/Navbar.tsx
@@ -0,0 +1,48 @@
+"use client";
+import React, { useState } from "react";
+import Link from "next/link";
+import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
+
+export default function Navbar() {
+  const [nav, setNav] = useState(true);
+  const handleNav = () => {
+    setNav(!nav);
+  };
+
+  return (
+    <div className="flex justify-between items-center h-20 max-w-screen mx-auto px-5 text-white pb-3 mb-3 bg-blue-500">
+      <h1 className="w-full text-3xl font-bold"> Fakebook </h1>
+      <ul className="hidden md:flex">
+        <li className="p-4">
+          <Link href="/"> Login</Link>
+        </li>
+        <li className="p-4">
+          <Link href="/about">About</Link>
+        </li>
+        <li className="p-4">Projects </li>
+      </ul>
+
+      <div onClick={handleNav} className="block md:hidden">
+        {!nav ? <AiOutlineClose size={30} /> : <AiOutlineMenu size={30} />}
+      </div>
+      <div
+        className={
+          !nav
+            ? "fixed left-0 top-0 w-[50%] h-full border-r border-r-blue-300 bg-blue-300 ease-in-out duration-500"
+            : "fixed left-[-100%]"
+        }
+      >
+        <h1 className="w-full text-3xl font-bold m-4 pt-4"> Fakebook </h1>
+        <ul className="uppercase p-2">
+          <Link href="/">
+            <li className="p-4 border-b border-blue-400"> Login </li>
+          </Link>
+          <Link href="/about">
+            <li className="p-4 border-b border-blue-400"> About </li>
+          </Link>
+          <li className="p-4 border-b border-blue-400"> Projects </li>
+        </ul>
+      </div>
+    </div>
+  );
+}
